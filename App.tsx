dace1cf8e03bb10df1389074080f449129fb588d
Joshua Amaju <princejosh0007@gmail.com>
2022-04-15 13:44:35 +0100

    theming

--- App.tsx
+++ App.tsx
@@ -1,18 +1,71 @@
+import * as eva from '@eva-design/eva';
+import {
+  ApplicationProvider,
+  Layout,
+  Text,
+  TopNavigation,
+} from '@ui-kitten/components';
 import React from 'react';
-import {StatusBar, Text, useColorScheme, View} from 'react-native';
+import {View} from 'react-native';
+import {Link, NativeRouter, Route, Routes} from 'react-router-native';
+import BackHandler from './shared/components/BackHandler';
+import * as header from './shared/components/Header';
+import evaTheme from './theme/eva.json';
+import mapping from './theme/mapping.json';
 
-const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
+const Home = () => {
   return (
-    <>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+    <Layout style={{flex: 1}}>
+      <header.Container>
+        <header.Title>Home</header.Title>
+      </header.Container>
 
       <View>
-        <Text>Hello World</Text>
+        <Link to="/about">
+          <Text>Go to about</Text>
+        </Link>
+
+        <Link to="/">
+          <Text>Go to Nested 1</Text>
+        </Link>
+
+        <Link to="/2">
+          <Text>Go to Nested 2</Text>
+        </Link>
+
+        <Routes>
+          <Route path="/" element={<Text>Nested 1</Text>} />
+          <Route path="/2" element={<Text>Nested 2</Text>} />
+        </Routes>
       </View>
-    </>
+    </Layout>
   );
 };
 
-export default App;
+const About = () => {
+  return (
+    <Layout style={{flex: 1}}>
+      <TopNavigation title="About" />
+
+      <Text>About</Text>
+    </Layout>
+  );
+};
+
+export default function App() {
+  return (
+    <ApplicationProvider
+      {...eva}
+      customMapping={mapping as any}
+      theme={{...eva.dark, ...evaTheme}}>
+      <NativeRouter>
+        <BackHandler />
+
+        <Routes>
+          <Route path="/*" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </NativeRouter>
+    </ApplicationProvider>
+  );
+}
